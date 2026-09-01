37061cab5ff448b898fae75d172d33cbf3c0affd
LuisAlvaradoRivas <lalvarado.rivas3134@gmail.com>
2026-08-31 22:00:22 -0700

    I added a timer :p

--- Assets/Scripts/Game State/GameState.cs
+++ Assets/Scripts/Game State/GameState.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
-
+using TMPro;
 public class GameState : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+     [SerializeField] TextMeshProUGUI timerText;
+     float elapsedTime;
     void Start()
     {
         
@@ -11,7 +13,12 @@ public class GameState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         elapsedTime+=Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        int milliseconds = Mathf.FloorToInt((elapsedTime * 1000) % 1000);
+
+        timerText.text = string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, milliseconds);
     }
     public void PlayerDeath()
     {
